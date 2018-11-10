83045316db73536932c6b01c898b05ed285b116d
Mr.Cookie <berheev99@gmail.com>
2018-11-10 10:39:51 +0400

    Сохранение

--- Lab1/Lab1/Air.cs
+++ Lab1/Lab1/Air.cs
@@ -70,16 +70,16 @@ namespace Lab1
             Brush brGray = new SolidBrush(MainColor);
             //крылья
             Pen gray = new Pen(Color.Gray, 10);
-            g.DrawLine(gray, _startPosX + 30, _startPosY, _startPosX + 55, _startPosY + 40);
-            g.DrawLine(gray, _startPosX + 30, _startPosY + 80, _startPosX + 55, _startPosY + 40);
+            g.DrawLine(gray, _startPosX + 40, _startPosY, _startPosX + 65, _startPosY + 40);
+            g.DrawLine(gray, _startPosX + 40, _startPosY + 75, _startPosX + 65, _startPosY + 40);
 
             // теперь отрисуем основной кузов аэробуса
             Pen grayPen = new Pen(Color.Gray, 10);
-            g.DrawEllipse(pen, _startPosX, _startPosY + 20, 100, 15);
-            g.FillEllipse(brGray, _startPosX, _startPosY + 20, 100, 15);
+            g.DrawEllipse(pen, _startPosX - 5, _startPosY + 25, 100, 20);
+            g.FillEllipse(brGray, _startPosX + 10, _startPosY + 25, 100, 20);
 
             //хвост
-            g.DrawLine(grayPen, _startPosX - 15, _startPosY, _startPosX, _startPosY + 25);
+            g.DrawLine(grayPen, _startPosX, _startPosY, _startPosX + 15, _startPosY + 25);
             
             // рисуем окно аэробуса
             Pen white = new Pen(Color.White, 3);
